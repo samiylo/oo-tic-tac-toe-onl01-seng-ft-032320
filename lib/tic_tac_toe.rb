632120481381775f7ab7b9e1d87862c8b0eb4aa3
Samiylo Kryshu <skryshu@gmail.com>
2020-04-03 18:53:31 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -3,7 +3,14 @@ class TicTacToe
   
   WIN_COMBINATIONS = [
     
-    
+    [0,1,2], # Top row
+    [3,4,5],  # Middle row
+    [6,7,8],  # Bottom row
+    [0,3,6],  # Left col
+    [1,4,7],  # Middle col
+    [2,5,8],  # Right col
+    [0,4,8],  # Diagnol 1
+    [2,4,6]
     
     ]
   
