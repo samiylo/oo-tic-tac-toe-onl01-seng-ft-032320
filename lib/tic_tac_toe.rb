e1996fbde0b65f86247531b2721df61fc937913b
Samiylo Kryshu <skryshu@gmail.com>
2020-04-03 20:04:06 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -16,7 +16,15 @@ class TicTacToe
   
   
   def initialize(board = nil)
-    @board = board ||Array.new(9, " ") 
+    # @board = board ||Array.new(9, " ") 
+  end
+  
+  def play
+    @board = Array.new(9, " ")
+    turn until over?
+    won? ? puts("Congratulations #{winner}!") : puts("Cat's Game!")
+    puts "Would you like to play again? (Y or N)"
+    gets.strip.downcase == "y" || gets.strip.downcase == "yes" ? play : puts("Goodbye!")
   end
   
   def display_board
