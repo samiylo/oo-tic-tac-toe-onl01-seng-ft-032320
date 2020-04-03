21bd147b12c5e48c55dd79c6a2e58dfc2bf685a5
Samiylo Kryshu <skryshu@gmail.com>
2020-04-03 20:21:34 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -68,11 +68,15 @@ class TicTacToe
   end
   
   def won?
-    
+    WIN_COMBINATIONS.any? do |combo|
+      if position_taken?(combo[0]) && @board[combo[0]] == @board[combo[1]] && @board[combo[1]] == @board[combo[2]]
+        return combo
+      end
+    end
   end
   
   def full?
-    !@board.any?{|item| item == "" || item == " "}
+    @board.all?{|item| item != " "}
   end
   
   def draw?
