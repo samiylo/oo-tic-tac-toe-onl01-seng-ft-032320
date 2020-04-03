6957977992a81d74ed68e02dd5c05ce517cc994d
Samiylo Kryshu <skryshu@gmail.com>
2020-04-03 19:26:19 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -50,7 +50,12 @@ class TicTacToe
     input = gets.strip
     index = input_to_index(input) 
     player = current_player
-    
+    if valid_move?(index)
+      move(index, player)
+      display_board
+    else
+      turn
+    end
   end
   
 end
