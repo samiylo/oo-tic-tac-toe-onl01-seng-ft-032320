2c2a21e94c8aa97797b2ba3037604e2c80b48103
Samiylo Kryshu <skryshu@gmail.com>
2020-04-03 19:35:23 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -67,7 +67,15 @@ class TicTacToe
   end
   
   def won
-    
+    one = WIN_COMBINATIONS.find{
+      |combo|
+      @board[combo[0]] == "X" && @board[combo[1]] == "X" && @board[combo[2]] == "X"
+    }
+    two = WIN_COMBINATIONS.find{
+      |combo|
+      @board[combo[0]] == "O" && @board[combo[1]] == "O" && @board[combo[2]] == "O"
+    }
+    return one || two
   end
   
 end
